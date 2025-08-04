97c55b17826b05704af6d790a9ad120110f7e014
IT GUY <61753646+ridhoo-code@users.noreply.github.com>
2025-08-04 16:20:31 +0700

    Update monitor.js

--- monitor.js
+++ monitor.js
@@ -2,7 +2,7 @@ const hosts = [
   { name: "BHAKTI MY COMPANY", url: "www.bhakti.co.id/myCompany/" },
   { name: "BHAKTI PURNA JUAL", url: "www.bhaktipurnajual.com" },
   { name: "ZEN", url: "zen.bhakti.co.id" },
-  { name: "MISHIRIN", url: "http://mishirin.com:8088/sales/login" },
+  { name: "MISHIRIN", url: "mishirin.com:8088" },
   { name: "EMAIL", url: "webmail.bhakti.co.id" },
 ];
 
